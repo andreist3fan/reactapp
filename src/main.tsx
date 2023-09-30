49a7b38cc0642682953f6961d40dbd3b4640abe6
Andrei Stefan <A.Stefan-1@student.tudelft.nl>
2023-09-30 19:16:58 +0200

    sadaf

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-// import App from "./App.tsx";
+//import App from "./App.tsx";
 import AppRouter from "./AppRouter.tsx";
 import "bootstrap/dist/css/bootstrap.css";
 
